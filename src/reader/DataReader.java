c8b908fb6403eb051f7c9a63e4dd860bf4d5b71a
asefrahman <asef1357@gmail.com>
2016-08-20 01:44:58 -0400

    Asef Exam

--- src/reader/DataReader.java
+++ src/reader/DataReader.java
@@ -1,5 +1,8 @@
 package reader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class DataReader {
 
 	public static void main(String[] args) {
@@ -13,7 +16,22 @@ public class DataReader {
 		 */
 
 		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
-	
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader(textFile);
+			br = new BufferedReader(fr);
+			String text = "";
+			while ((text = br.readLine()) != null) {
+				System.out.println(text);
+			}
+		} catch (Exception ex) {
+			System.out.println("Hey ! file is not found. fix the path");
+		} finally {
+			fr.close();
+			br.close();
+
+		}
 	}
 
 }
